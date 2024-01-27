440c45184ead7fb20135a001aef6a605e3dea1de
Abhishek Dangi <72682273+Abhi-AD@users.noreply.github.com>
2024-01-27 16:39:28 +0545

    Abhi-AD Ecommerce

---
+++ store/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from store.models import Product
+
+# Create your views here.
+def home(request):
+     products = Product.objects.all()
+     return render(request, 'home.html', {'products':products})
+
+def about(request):
+     return render(request, 'about.html') 
